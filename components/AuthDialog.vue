0c94761098a67027ce75760d3c78a00b2d8dcaa9
Zhang Xiaoyu <zhangxiaoyu1017@gmail.com>
2024-07-05 16:48:03 +0800

    feat: Update AuthDialog

--- components/AuthDialog.vue
+++ components/AuthDialog.vue
@@ -1,13 +1,13 @@
 <template>
     <v-dialog v-model="authStore.showAuthDialog" persistent max-width="400px">
         <v-card>
-            <v-card-title class="headline">密码验证</v-card-title>
+            <v-card-title class="headline">Password Verification</v-card-title>
             <v-card-text>
-                <v-text-field v-model="password" label="请输入密码" type="password" @keyup.enter="verifyPassword" />
+                <v-text-field v-model="password" label="Please enter password" type="password" @keyup.enter="verifyPassword" />
             </v-card-text>
             <v-card-actions>
-                <v-btn color="primary" @click="verifyPassword">确认</v-btn>
-                <v-btn color="secondary" @click="cancel">取消</v-btn>
+                <v-btn color="primary" @click="verifyPassword">Confirm</v-btn>
+                <v-btn color="secondary" @click="cancel">Cancel</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
